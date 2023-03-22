1fb91867bd750e70c558bbff675a8825c2c72e8f
jamesbryer <jamesbryer098@gmail.com>
2023-03-22 20:52:50 +0000

    emergency commit

---
+++ add-to-map/index.php
@@ -0,0 +1,18 @@
+<?php
+include "../header.php";
+?>
+
+
+
+
+<body>
+  <div class="container-fluid" id="map"></div>
+  <br />
+  <button id="submit-btn" class="btn btn-outline-secondary">Submit</button>
+  <button id="reset-btn" class="btn btn-outline-secondary">Reset</button>
+  <script src="script.js"></script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtwLJf7L22miHaKQlI9StgOIpa2XdYaPk&callback=initMap">
+  </script>
+</body>
+
+</html>
